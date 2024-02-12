533c3cbe63d9f0af920e926404450b43fe54024e
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-12 15:02:20 +0000

    conversion

---
+++ basics/conversionOperations.js
@@ -0,0 +1,21 @@
+// let score = "33xyz"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Nan => Not a number(type- number)
+
+// let isLogged = null
+// console.log(typeof isLogged);
+// let boolisLogged = Boolean(isLogged)
+// console.log(boolisLogged);
+
+let num = 1000
+console.log(typeof num);
+let strnum = String(num)
+console.log(typeof strnum);
+console.log(strnum);
