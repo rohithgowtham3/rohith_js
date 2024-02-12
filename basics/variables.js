3e3eff1a997db4dcb9d404f150a318a92f188ebc
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-12 14:09:36 +0000

    variables pushed

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "roro@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mysore"
+let accState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accState])
