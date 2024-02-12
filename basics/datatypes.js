6183b67b1906a446c34415e90a57a1cadd4b6a94
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-12 14:23:54 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3 + 3) //this works only in the console of the browser, the one used here is nodejs
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared, value not defined
+// symbol => unique
+// object
+
+console.log(typeof "rohith");
+console.log(typeof null);
