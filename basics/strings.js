f8cb38fa88a7e54541f6fad0e9fd320358db1063
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-13 13:41:00 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,16 @@
+const name = 'rohith-g'
+// let projCount = 10
+
+// console.log(`${name}'s repository count is ${projCount}`);
+
+// console.log(name.toUpperCase());
+// console.log(name.toLocaleUpperCase());
+
+const newname = name.substring(0, 4);
+console.log(newname);
+
+const newString = name.slice(-8, 4)
+
+let url = "https://rohithgowtham/%20rohith.js                 "
+console.log(url.replace('%20', '-'));
+console.log(url.trim());
