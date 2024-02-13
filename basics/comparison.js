78da47de1e17e85ab8495f437ac68acf2b1f316f
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-13 08:37:43 +0000

    summary of data types

---
+++ basics/comparison.js
@@ -0,0 +1,15 @@
+/* Primittive
+
+7 types: String, Number, Boolean, null, undefined, Symbol(used to make any value a unique one), Bigint
+
+*/
+
+// Reference(Non-primittive)
+
+// Array, Object, Functions
+
+const myFunc = function(){
+    console.log("hello!");
+}
+
+myFunc();
