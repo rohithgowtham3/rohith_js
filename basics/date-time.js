75ecf99461cf4acd847a3fe723571a059abfd1b4
rohithgowtham3 <136837690+rohithgowtham3@users.noreply.github.com>
2024-02-16 12:00:10 +0000

    date

---
+++ basics/date-time.js
@@ -0,0 +1,12 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(typeof myDate.toISOString());
+
+// console.log(typeof myDate);
+
+// let myDate2 = new Date(2023, 14, 11)
+// console.log(myDate2.toISOString());
+
+let newDate = new Date()
+console.log(newDate.getFullYear());
